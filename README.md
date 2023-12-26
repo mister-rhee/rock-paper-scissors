7f8b1d2024185b6db2aca647bbe01473c739a075
Richard Rhee <rdrhee@gmail.com>
2023-12-25 20:27:04 -0800

    Add screenshots to repo and update readme

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Rock Paper Scissors
 
-Created in HTML, CSS, JS
+Simple rock paper scissors browser game created in HTML, CSS, JS\
+First to five points wins!
+
+## start screen
+![start screen](screenshots/first_screen.png)
+
+## player gets a point
+![player point](screenshots/player_point.png)
+
+## opponent gets a point
+![opponent point](screenshots/opponent_point.png)
+
+## player and opponent tie
+![tie](screenshots/tie.png)
+
+## player wins game
+![player win](screenshots/youwin.png)
+
+## opponent wins game
+![opponent win](screenshots/youlose.png)
--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-const FIRST_TO = 5;
-
 function getComputerChoice() {
     choice = Math.floor(Math.random() * 3);
     updateComputerDisplay(choice);
@@ -14,51 +12,95 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "tie";
+    }
     if (playerSelection == "ROCK") {
-        if (computerSelection == PAPER) {
-            cond = false;
-            return 0;
-        } else {
-            cond = false;
-            return 1;
+        if (computerSelection == "PAPER") {
+            return "opponent";
+        } else if (computerSelection == "SCISSORS") {
+            return "player";
         }
     } else if (playerSelection == "PAPER") {
-        if (computerSelection == SCISSORS) {
-            cond = false;
-            return 0;
-        } else {
-            cond = false;
-            return 1;
+        if (computerSelection == "SCISSORS") {
+            return "opponent";
+        } else if (computerSelection == "ROCK") {
+            return "player";
         }
     } else if (playerSelection == "SCISSORS") {
-        if (computerSelection == ROCK) {
-            cond = false;
-            return 0;
-        } else {
-            cond = false;
-            return 1;
+        if (computerSelection == "ROCK") {
+            return "opponent";
+        } else if (computerSelection == "PAPER") {
+            return "player";
         }
     }
 }
 
 function updateComputerDisplay(comp) {
     switch (comp) {
-        case 0: computerDisplay.textContent = "ROCK"; break;
-        case 1: computerDisplay.textContent = "PAPER"; break;
-        case 2: computerDisplay.textContent = "SCISSORS"; break;
+        case 0: computerDisplay.textContent = "🪨"; break;
+        case 1: computerDisplay.textContent = "📝"; break;
+        case 2: computerDisplay.textContent = "✂️"; break;
     }
-    return;
 }
 
 function updatePlayerDisplay(player) {
-    playerDisplay.textContent = player;
+    switch (player) {
+        case "ROCK": playerDisplay.textContent = "🪨"; break;
+        case "PAPER": playerDisplay.textContent = "📝"; break;
+        case "SCISSORS": playerDisplay.textContent = "✂️"; break;
+    }
+}
+
+function updateScoreboard() {
+    playerScore.textContent = `You: ${scorePlayer}`;
+    computerScore.textContent = `Opponent: ${scoreComputer}`;
 }
 
 function handleClick(player) {
-    if (scorePlayer < 5 && scoreComputer < 5) {
-        updatePlayerDisplay(player);
-        playRound(player, getComputerChoice());
+    updatePlayerDisplay(player);
+    pointWinner = playRound(player, getComputerChoice());
+    if (pointWinner == "player") {
+        scorePlayer++;
+    } else if (pointWinner == "opponent") {
+        scoreComputer++;
     }
+    whoGotPoint(pointWinner);
+    updateScoreboard();
+    if (scorePlayer >= 5 || scoreComputer >= 5) {
+        gameOver();
+    }
+}
+
+function whoGotPoint(pointWinner) {
+    switch (pointWinner) {
+        case "player": subtext.textContent = "You got a point!"; break;
+        case "opponent": subtext.textContent = "Opponent got a point!"; break;
+        case "tie": subtext.textContent = "It's a tie!"
+    }
+}
+
+function gameOver() {
+    btnRock.disabled = true;
+    btnPaper.disabled = true;
+    btnSci.disabled = true;
+    if (scorePlayer == 5) {
+        subtext.textContent = "You Win!";
+    } else { subtext.textContent = "You Lose!" }
+    btnReset.style.visibility = 'visible';
+}
+
+function resetGame() {
+    subtext.textContent = "First to 5 wins!";
+    playerDisplay.textContent = "?";
+    computerDisplay.textContent = "?";
+    scorePlayer = 0;
+    scoreComputer = 0;
+    updateScoreboard();
+    btnRock.disabled = false;
+    btnPaper.disabled = false;
+    btnSci.disabled = false;
+    btnReset.style.visibility = "hidden";
 }
 
 let scorePlayer = 0;
@@ -67,8 +109,13 @@ const btnRock = document.querySelector(".rock");
 const btnPaper = document.querySelector(".paper");
 const btnSci = document.querySelector(".scissors");
 const playerDisplay = document.querySelector(".playerDisplay");
+const playerScore = document.querySelector(".playerScore");
 const computerDisplay = document.querySelector(".computerDisplay");
+const computerScore = document.querySelector(".computerScore");
+const subtext = document.querySelector(".subtext");
+const btnReset = document.querySelector(".reset");
 
 btnRock.addEventListener('click', () => handleClick("ROCK"));
 btnPaper.addEventListener('click', () => handleClick("PAPER"));
 btnSci.addEventListener('click', () => handleClick("SCISSORS"));
+btnReset.addEventListener('click', () => resetGame());
